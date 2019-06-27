4adb092db2db3b499c10e70c91417a9b0ddeeeaf
Lena <sulyh9@gmail.com>
2019-06-27 15:16:42 +1000

    passed all the example of test

--- lib/surveyor/rating_question.rb
+++ lib/surveyor/rating_question.rb
@@ -1,5 +1,11 @@
 module Surveyor
   class RatingQuestion < Question
     # your code goes here
+
+    VALID_NUMBER = (1..5)
+
+    def valid_answer?(number)
+      VALID_NUMBER.include?(number) ? true : false
+    end
   end
 end
